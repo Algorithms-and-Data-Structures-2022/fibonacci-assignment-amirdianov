6d2168229ea8728053ed792e17fc5ed4f053e5cb
diyanovamir <amirdianov@gmail.com>
2022-04-19 22:26:09 +0300

    Homework (all done)

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -11,9 +11,9 @@ namespace assignment {
       return n;
     }
 
-    // Напишите здесь свой код ...
+    return round((pow(((1 + sqrt(5)) / 2), n) - pow(((1 - sqrt(5)) / 2), n)) / sqrt(5));
 
-    return 0;
+    //    return 0;
   }
 
   int64_t fib_iterative(int n) {
@@ -26,14 +26,26 @@ namespace assignment {
     int64_t fib_prev = 0;
     int64_t fib_curr = 1;
 
-    // Напишите здесь свой код ...
+    int prev = fib_prev;
+    int now = fib_curr;
+    int perem;
+    for (int i = 1; i < n + 1; i++) {
+      perem = now;
+      now = now + prev;
+      prev = perem;
+    }
+    return perem;
 
     return 0;
   }
 
   int64_t fib_recursive(int n) {
 
-    // Напишите здесь свой код ...
+    if (n <= 1) {
+      return n;
+    } else {
+      return fib_recursive(n - 1) + fib_recursive(n - 2);
+    }
 
     return 0;
   }
@@ -45,10 +57,11 @@ namespace assignment {
     }
 
     if (cache[n] != -1) {
-      // Напишите здесь свой код ...
-      return 0;
+      return cache[n];
     }
-
+    int val = fib_recursive_memoization(n - 1, cache) + fib_recursive_memoization(n - 2, cache);
+    cache[n] = val;
+    return val;
     // Напишите здесь свой код ...
 
     return 0;
@@ -61,7 +74,7 @@ namespace assignment {
     }
 
     // Напишите здесь свой код ...
-
+    return matrix_power(FibMatrix, n)[1][0];
     // Tip: используйте реализованную функцию matrix_pow
 
     return 0;
